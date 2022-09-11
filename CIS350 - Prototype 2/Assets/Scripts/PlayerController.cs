100823cb89c6d812c855fab9433a41133915e32c
Lucas Johnson <lwjohnson@mail.bradley.edu>
2022-09-10 20:04:18 -0500

    Prototype 2 Changes Part 1
    (1:09 min in video so far)

---
+++ CIS350 - Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿/*
+* Lucas Johnson
+* Prototype 2
+* Controls player movement)
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 14;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
---
+++ CIS350 - Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] prefabsToSpawn;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            int prefabIndex = Random.Range(0, prefabsToSpawn.Length);
+
+            Instantiate(prefabsToSpawn[prefabIndex], new Vector3(0, 0, 20), prefabsToSpawn[prefabIndex].transform.rotation);
+        }
+    }
+}
