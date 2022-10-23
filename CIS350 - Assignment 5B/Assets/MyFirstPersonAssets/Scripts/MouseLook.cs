4af98811b9f1ab1249a1fccca84fcd684d5e6695
Lucas Johnson <lwjohnson@mail.bradley.edu>
2022-10-23 17:29:09 -0500

    Assignment 5B Complete

---
+++ CIS350 - Assignment 5B/Assets/MyFirstPersonAssets/Scripts/MouseLook.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Lucas Johnson
+ * Assignment 5B
+ * Controls mouse/camera movement
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public GameObject player;
+
+    private float verticalLookRotation = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        player.transform.Rotate(Vector3.up * mouseX);
+
+        verticalLookRotation -= mouseY;
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90, 90);
+        transform.localRotation = Quaternion.Euler(verticalLookRotation, 0f, 0f);
+
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
---
+++ CIS350 - Assignment 5B/Assets/MyFirstPersonAssets/Scripts/ShootWithRaycasts.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Lucas Johnson
+ * Assignment 5B
+ * Controls shooting
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootWithRaycasts : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public float hitForce = 10f;
+    public Camera cam;
+
+    public ParticleSystem muzzleFlash;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+
+        RaycastHit hitInfo;
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo, range))
+        {
+            Debug.Log(hitInfo.transform.name);
+
+            Target target = hitInfo.transform.gameObject.GetComponent<Target>();
+
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if(hitInfo.rigidbody != null)
+            {
+                hitInfo.rigidbody.AddForce(cam.transform.TransformDirection(Vector3.forward) * hitForce, ForceMode.Impulse);
+            }
+        }
+    }
+}
