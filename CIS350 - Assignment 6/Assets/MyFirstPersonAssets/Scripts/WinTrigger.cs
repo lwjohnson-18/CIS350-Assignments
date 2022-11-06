bea67e027a2812d2f961bd5cb4611e5e4cc7f4b9
Lucas Johnson <lwjohnson@mail.bradley.edu>
2022-11-06 15:25:57 -0600

    Assignment 6 Init Files

---
+++ CIS350 - Assignment 6/Assets/MyFirstPersonAssets/Scripts/WinTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinTrigger : MonoBehaviour
+{
+    public GameObject winText;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag.Equals("Player"))
+        {
+            winText.SetActive(true);
+        }
+    }
+}
