c17e640224617ea01d874c11ff3bbaeced2ae882
Lucas Johnson <lwjohnson@mail.bradley.edu>
2022-11-26 22:58:41 -0600

    Prototype 5 Complete

---
+++ CIS350 - Prototype 5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * Lucas Johnson
+ * Prototype 5
+ * Controls the overal game management
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+
+    private float spawnRate = 1.0f;
+
+    public TextMeshProUGUI scoreText;
+    private int score;
+
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+
+    public bool isGameActive = false;
+
+    public GameObject titleScreen;
+
+    public void StartGame(int difficulty)
+    {
+        spawnRate /= difficulty;
+
+        isGameActive = true;
+        score = 0;
+        UpdateScore(0);
+        StartCoroutine(SpawnTarget());
+        titleScreen.SetActive(false);
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while(isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ CIS350 - Prototype 5/Assets/Scripts/Target.cs
@@ -0,0 +1,72 @@
+﻿/*
+ * Lucas Johnson
+ * Prototype 5
+ * Controls Target Behavior
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+    private GameManager gameManager;
+
+    private float minSpeed = 12f;
+    private float maxSpeed = 16f;
+    private float maxTorque = 10f;
+    private float xRange = 4f;
+    private float ySpawnPos = -6f;
+
+    public int pointValue;
+
+    public ParticleSystem explosionParticle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
+
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        transform.position = RandomSpawnPos();
+    }
+
+    private Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    private float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    private Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+
+    private void OnMouseDown()
+    {
+        if (gameManager.isGameActive)
+        {
+            gameManager.UpdateScore(pointValue);
+
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+        Destroy(gameObject);
+    }
+}
