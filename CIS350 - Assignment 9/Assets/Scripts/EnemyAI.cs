4f531650635850bd55ad02630b4f9fd9d925bcf5
Lucas Johnson <lwjohnson@mail.bradley.edu>
2022-12-10 17:42:24 -0600

    Assignment 9 Complete

---
+++ CIS350 - Assignment 9/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * Lucas Johnson
+ * Assignment 9
+ * Controls the Enemy
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+    public ThirdPersonCharacter character;
+
+    public GameObject player;
+
+    float chaseDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+
+        character = GetComponent<ThirdPersonCharacter>();
+        agent.updateRotation = false;
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        chaseDistance = 5.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveEnemy();
+    }
+
+    private void MoveEnemy()
+    {
+        float distanceFromTarget = Vector3.Distance(transform.position, player.transform.position);
+
+        if (distanceFromTarget > agent.stoppingDistance && distanceFromTarget < chaseDistance)
+        {
+            agent.SetDestination(player.transform.position);
+            character.Move(agent.desiredVelocity, false, false);
+        }
+        else
+        {
+            agent.SetDestination(transform.position);
+            character.Move(Vector3.zero, false, false);
+        }
+    }
+}
---
+++ CIS350 - Assignment 9/Assets/Scripts/MoveTo.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Lucas Johnson
+ * Assignment 9
+ * Moves the NavMesh Agent to set Transform
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+    public Transform goal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+}
